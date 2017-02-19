bdf151742d7d1b835698f3c47eddce0bc26b1df1
Abramov Anton <git@abramovanton.ru>
2017-02-19 19:44:01 +0300

    finish dev dish list(menu) load and view; start dev detail dish features; add comments and refactor code(spaces to tabs);

--- appProg/Form1.Designer.cs
+++ appProg/Form1.Designer.cs
@@ -28,49 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnAppClose = new System.Windows.Forms.Button();
-            this.menu = new System.Windows.Forms.TabControl();
-            this.SuspendLayout();
-            // 
-            // btnAppClose
-            // 
-            this.btnAppClose.Location = new System.Drawing.Point(831, 12);
-            this.btnAppClose.Name = "btnAppClose";
-            this.btnAppClose.Size = new System.Drawing.Size(75, 23);
-            this.btnAppClose.TabIndex = 1;
-            this.btnAppClose.Text = "Закрыть";
-            this.btnAppClose.UseVisualStyleBackColor = true;
-            this.btnAppClose.Click += new System.EventHandler(this.btnAppClose_Click);
-            // 
-            // menu
-            // 
-            this.menu.Location = new System.Drawing.Point(12, 12);
-            this.menu.Name = "menu";
-            this.menu.SelectedIndex = 0;
-            this.menu.Size = new System.Drawing.Size(454, 559);
-            this.menu.TabIndex = 2;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(918, 656);
-            this.Controls.Add(this.menu);
-            this.Controls.Add(this.btnAppClose);
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
-            this.Name = "Form1";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
-            this.Text = "Кафе";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.Load += new System.EventHandler(this.Form1_Load);
-            this.ResumeLayout(false);
+			this.btnAppClose = new System.Windows.Forms.Button();
+			this.menu = new System.Windows.Forms.TabControl();
+			this.dishDetail = new System.Windows.Forms.TabControl();
+			this.searchBtn = new System.Windows.Forms.Button();
+			this.searchInput = new System.Windows.Forms.TextBox();
+			this.searchLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnAppClose
+			// 
+			this.btnAppClose.Location = new System.Drawing.Point(831, 12);
+			this.btnAppClose.Name = "btnAppClose";
+			this.btnAppClose.Size = new System.Drawing.Size(75, 23);
+			this.btnAppClose.TabIndex = 1;
+			this.btnAppClose.Text = "Закрыть";
+			this.btnAppClose.UseVisualStyleBackColor = true;
+			this.btnAppClose.Click += new System.EventHandler(this.btnAppClose_Click);
+			// 
+			// menu
+			// 
+			this.menu.Location = new System.Drawing.Point(12, 41);
+			this.menu.Name = "menu";
+			this.menu.SelectedIndex = 0;
+			this.menu.Size = new System.Drawing.Size(454, 530);
+			this.menu.TabIndex = 2;
+			this.menu.SelectedIndexChanged += new System.EventHandler(this.menu_SelectedIndexChanged);
+			// 
+			// dishDetail
+			// 
+			this.dishDetail.Location = new System.Drawing.Point(472, 41);
+			this.dishDetail.Name = "dishDetail";
+			this.dishDetail.SelectedIndex = 0;
+			this.dishDetail.Size = new System.Drawing.Size(434, 257);
+			this.dishDetail.TabIndex = 3;
+			// 
+			// searchBtn
+			// 
+			this.searchBtn.Location = new System.Drawing.Point(338, 12);
+			this.searchBtn.Name = "searchBtn";
+			this.searchBtn.Size = new System.Drawing.Size(75, 23);
+			this.searchBtn.TabIndex = 4;
+			this.searchBtn.Text = "Поиск";
+			this.searchBtn.UseVisualStyleBackColor = true;
+			// 
+			// searchInput
+			// 
+			this.searchInput.Location = new System.Drawing.Point(100, 14);
+			this.searchInput.Name = "searchInput";
+			this.searchInput.Size = new System.Drawing.Size(196, 20);
+			this.searchInput.TabIndex = 5;
+			// 
+			// searchLabel
+			// 
+			this.searchLabel.AutoSize = true;
+			this.searchLabel.Location = new System.Drawing.Point(22, 17);
+			this.searchLabel.Name = "searchLabel";
+			this.searchLabel.Size = new System.Drawing.Size(39, 13);
+			this.searchLabel.TabIndex = 6;
+			this.searchLabel.Text = "Поиск";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(918, 656);
+			this.Controls.Add(this.searchLabel);
+			this.Controls.Add(this.searchInput);
+			this.Controls.Add(this.searchBtn);
+			this.Controls.Add(this.dishDetail);
+			this.Controls.Add(this.menu);
+			this.Controls.Add(this.btnAppClose);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.Text = "Кафе";
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			this.Load += new System.EventHandler(this.onLoad);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Button btnAppClose;
         private System.Windows.Forms.TabControl menu;
-    }
+		private System.Windows.Forms.TabControl dishDetail;
+		private System.Windows.Forms.Button searchBtn;
+		private System.Windows.Forms.TextBox searchInput;
+		private System.Windows.Forms.Label searchLabel;
+	}
 }
 
--- appProg/Form1.cs
+++ appProg/Form1.cs
@@ -1,313 +1,382 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using MySql.Data.MySqlClient;
-using Newtonsoft.Json;
 
 namespace appProg
 {
-    public partial class Form1 : Form
-    {
-        private List<string> menuSections;
-        private DataGridView[] menuTables;
+	public partial class Form1 : Form
+	{
+		private List<string> menuSections;
+		private DataGridView[] menuTables;
 
-        int height = Screen.PrimaryScreen.Bounds.Height;
-        int width = Screen.PrimaryScreen.Bounds.Width;
+		int height = Screen.PrimaryScreen.Bounds.Height;
+		int width = Screen.PrimaryScreen.Bounds.Width;
 
-        public Form1()
-        {
-            InitializeComponent();
+		public Form1()
+		{
+			InitializeComponent();
 
-            /**
-             * Form settings
-             * */
+			/**
+			 * Form settings
+			 * */
 
+			// left part window = cafe's menu
+			this.menu.Width = width / 2 - 60;
+			this.menu.Height = height - 40 - 20;
+			this.menu.Location = new Point(
+				20,
+				40
+			);
 
-            this.menu.Width = width / 2 - 60;
-            this.menu.Height = height - 40 - 20;
-            this.menu.Location = new Point(
-                20,
-                40
-            );
+			//exit btn
+			this.btnAppClose.Location = new Point(
+				width - btnAppClose.Width - 20,
+				15
+			);
 
-            /* dataGridView1.Width = width / 2 - 60;
-             dataGridView1.Height = height - 40 - 20;
-             dataGridView1.Location = new Point(
-                 20,
-                 40
-             );*/
+			//detail dish wrapper
+			this.dishDetail.Width = width / 2 - 30;
+			this.dishDetail.Height = height / 2;
+			this.dishDetail.Location = new Point(
+				width / 2,
+				40
+			);
 
+			//TODO set properties for search
+			/**
+			 * Form settings
+			 * */
 
-            this.btnAppClose.Location = new Point(
-                width - btnAppClose.Width - 20,
-                20
-            );
+			/**
+			 * Get data from DB
+			 * */
 
-            menuSections = DB.getMenuSections();
-            createTabs(menuSections);
-            fillTab(0, getTabDataByName(menuSections[0]));
-        }
+			menuSections = DB.getMenuSections(); // get all exist sections
+			createTabs(menuSections); // create tabs by found sections
+			
+			loadTabDishes(0); // fill data to first tab
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            /**
-             * Убираем границы окна и делаем его на весь экран
-             * */
-            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            Left = Top = 0;
-            Width = Screen.PrimaryScreen.Bounds.Width;
-            Height = Screen.PrimaryScreen.Bounds.Height;
-        }
+			/**
+			 * Get data from DB
+			 * */
+		}
 
-        private prewiewDish getTabDataByName(string name)
-        {
-            prewiewDish dishes = DB.getTabDishesByName(name);
-            //dishes.Add(new prewiewDish { id = 1, name = "123", weight = 12, cost = 15.30f });
-            //dishes.Add(new prewiewDish { id = 2, name = "456", weight = 16, cost = 75.90f });
+		/**
+		 * On load main window
+		 * */
+		private void onLoad(object sender, EventArgs e)
+		{
+			/**
+			 * Hide windows's borders and resize to fullscreen
+			 * */
+			FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			Left = Top = 0;
+			Width = Screen.PrimaryScreen.Bounds.Width;
+			Height = Screen.PrimaryScreen.Bounds.Height;
+		}
 
+		/**
+		 * Func(wrapper) for load data from DB and fill to menu
+		 * */
+		private void loadTabDishes(int index)
+		{
+			prewiewDish dishes = DB.getTabDishesByName(menuSections[index]);
+			menuTables[index].Rows.Clear();
+			foreach (prewiewDish dish in dishes)
+				menuTables[index].Rows.Add(new object[] { dish.id, dish.name, dish.weight, dish.cost });
+		}
 
-            return dishes;
-        }
+		/**
+		 * Func for create tab on menu
+		 * */
+		private void createTabs(List<string> sections)
+		{
+			int menuHeight = this.menu.Height;
+			int menuWidth = this.menu.Width;
+			int countSections = sections.Count();
+			menuTables = new DataGridView[sections.Count];
+			
+			menuColumn[] columns = {
+				new menuColumn { name = "id", header = "ИД", width = menuWidth / 100 * 5 },
+				new menuColumn { name = "name", header = "Название", width = menuWidth / 100 * 65  - 40},
+				new menuColumn { name = "weight", header = "Вес, грамм", width = menuWidth / 100 * 15 },
+				new menuColumn { name = "cost", header = "Стоимость, руб.", width = menuWidth / 100 * 15 },
+			};
+			int countColumns = columns.Count();
+			
+			for (int i = 0; i < countSections; i++)
+			{
 
-        private void fillTab(int index, prewiewDish dishes)
-        {
-            foreach (prewiewDish dish in dishes)
-                menuTables[index].Rows.Add(new object[] { dish.id, dish.name, dish.weight, dish.cost });
-        }
+				TabPage myTabPage = new TabPage(sections[i]);
+				menuTables[i] = new DataGridView();
+				menuTables[i].Left = menuTables[i].Top = 20;
+				menuTables[i].Height = menuHeight - 60;
+				menuTables[i].Width = menuWidth - 40;
+				menuTables[i].RowHeadersVisible = false;
+				menuTables[i].ScrollBars = ScrollBars.Vertical;
+				menuTables[i].ReadOnly = true;
+				menuTables[i].AllowUserToAddRows = false;
+				menuTables[i].CellClick += new DataGridViewCellEventHandler(this.menuTableCell_Click);
 
-        private void createTabs(List<string> sections)
-        {
-            int menuHeight = this.menu.Height;
-            int menuWidth = this.menu.Width;
-            int countSections = sections.Count();
-            menuTables = new DataGridView[sections.Count];
-            
-            menuColumn[] columns = {
-                new menuColumn { name = "id", header = "№", width = menuWidth / 100 * 5 },
-                new menuColumn { name = "name", header = "Название", width = menuWidth / 100 * 65  - 40},
-                new menuColumn { name = "weight", header = "Вес, грамм", width = menuWidth / 100 * 15 },
-                new menuColumn { name = "cost", header = "Стоимость, руб.", width = menuWidth / 100 * 15 },
-            };
-            int countColumns = columns.Count();
-            
-            for (int i = 0; i < countSections; i++)
-            {
+				for (int j = 0; j < countColumns; j++)
+				{
+					DataGridViewColumn column = new DataGridViewTextBoxColumn();
+					column.Width = columns[j].width;
+					column.Name = columns[j].name;
+					column.HeaderText = columns[j].header;
+					menuTables[i].Columns.Add(column);
+				}
 
-                TabPage myTabPage = new TabPage(sections[i]);
-                menuTables[i] = new DataGridView();
-                menuTables[i].Left = menuTables[i].Top = 20;
-                menuTables[i].Height = menuHeight - 60;
-                menuTables[i].Width = menuWidth - 40;
-                menuTables[i].RowHeadersVisible = false;
-                menuTables[i].ScrollBars = ScrollBars.Vertical;
+				myTabPage.Controls.Add(menuTables[i]);
+				this.menu.TabPages.Add(myTabPage);
+			}
+		}
 
-                for (int j = 0; j < countColumns; j++)
-                {
-                    DataGridViewColumn column = new DataGridViewTextBoxColumn();
-                    column.Width = columns[j].width;
-                    column.Name = columns[j].name;
-                    column.HeaderText = columns[j].header;
-                    menuTables[i].Columns.Add(column);
-                }
+		/**
+		 * Button for close application
+		 * */
+		private void btnAppClose_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
 
-                myTabPage.Controls.Add(menuTables[i]);
-                this.menu.TabPages.Add(myTabPage);
-            }
-        }
+		/**
+		 * Select dish in menu(dish list)
+		 * */
+		private void menuTableCell_Click(object sender, DataGridViewCellEventArgs e)
+		{
+			int row = e.RowIndex;
+			if (row > -1) {
+				var tab = menu.SelectedIndex;
+				int id = (int)menuTables[tab].Rows[row].Cells[0].Value;
+				showDetailDish(id);
+			}
+		}
 
-        private void btnAppClose_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
+		/**
+		 * Load data about dish by ID and fill to detail dish block
+		 * */
+		private void showDetailDish(int id) {
+			detailDish dish = DB.getDishByID(id);
 
-        private void tabPage1_Click(object sender, EventArgs e)
-        {
+			MessageBox.Show("DISH NAME = " + dish.name);
+		}
 
-        }
-    }
+		/**
+		 * Change tab index in menu
+		 * */
+		private void menu_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			int tab = this.menu.SelectedIndex;
+			loadTabDishes(tab);
+		}
+	}
+	/**
+	 * Class for work with Mysql Data Base;
+	 * 
+	 * class DBResult - get response data from DB
+	 * 
+	 * */
+	public class DB
+	{
+		private static string database = "cafe";
+		private static string host = "127.0.0.1";
+		private static string user = "mysql";
+		private static string password = "mysql";
+		private MySqlConnection connection;
 
-    public class DB
-    {
-        private static string database = "cafe";
-        private static string host = "127.0.0.1";
-        private static string user = "mysql";
-        private static string password = "mysql";
-        private MySqlConnection connection;
+		public class DBResult
+		{
+			public bool success = false;
+			public string error = "Неизвестная ошибка.";
+			public List<List<Object>> data = new List<List<Object>>();
+		}
 
-        public class DBResult
-        {
-            public bool success = false;
-            public string error = "Неизвестная ошибка.";
-            public List<List<Object>> data = new List<List<Object>>();
-        }
+		public static List<string> getMenuSections()
+		{
+			DB db = new DB();
+			List<string> sections = new List<string>();
+			DBResult result = db.exec("SELECT type FROM dish ORDER BY CAST(type AS CHAR)");
 
-        public static List<string> getMenuSections()
-        {
-            DB db = new DB();
-            List<string> sections = new List<string>();
-            DBResult result = db.exec("SELECT type FROM dish ORDER BY CAST(type AS CHAR)");
+			if (result.success)
+			{
+				int rows = result.data.Count();
+				for(int i = 0; i < rows; i++)
+					sections.Add(result.data[i][0].ToString());
+			}
 
-            if (result.success)
-            {
-                int rows = result.data.Count();
-                for(int i = 0; i < rows; i++)
-                    sections.Add(result.data[i][0].ToString());
-            }
+			return sections;
+		}
 
-            return sections;
-        }
+		public static prewiewDish getTabDishesByName(string sectionName)
+		{
+			DB db = new DB();
+			prewiewDish dishes = new prewiewDish();
+			DBResult result = db.exec("SELECT id, name, weight, cost FROM dish WHERE type = '" + sectionName + "';");
 
-        public static prewiewDish getTabDishesByName(string sectionName)
-        {
-            DB db = new DB();
-            prewiewDish dishes = new prewiewDish();
-            DBResult result = db.exec("SELECT id, name, weight, cost FROM dish WHERE type = '" + sectionName + "';");
+			if (result.success)
+			{
+				int rows = result.data.Count();
+				for (int i = 0; i < rows; i++)
+				{
+					var row = result.data[i];
 
-            if (result.success)
-            {
-                int rows = result.data.Count();
-                for (int i = 0; i < rows; i++)
-                {
-                    var row = result.data[i];
+					var id = (int)row[0];
+					var name = row[1].ToString();
+					var weight = (float)row[2];
+					var cost = (float)row[3];
 
-                    var id = Convert.ToInt32(row[0]);
-                    var name = row[1].ToString();
-                    var weight = (float)row[2];
-                    var cost = (float)row[3];
+					dishes.Add(new prewiewDish { id = id, name = name, weight = weight, cost = cost });
+				}
+			}
+			return dishes;
+		}
 
-                    dishes.Add(new prewiewDish { id = id, name = name, weight = weight, cost = cost });
-                }
-               
-            }
-               
-            return dishes;
-        }
+		public static detailDish getDishByID(int id)
+		{
+			DB db = new DB();
+			DBResult result = db.exec("SELECT * FROM dish WHERE id = '" + id + "' LIMIT 1;");
+			detailDish dish = null;
 
+			if (result.success)
+			{
+				var row = result.data[0];
+				var name = row[1].ToString();
 
-        public void connect()
-        {
-            try
-            {
-                connection = this.create();
-            }
-            catch (MySqlException e)
-            {
-                Console.WriteLine(e);
-                MessageBox.Show("Не удалось создать подключение к базе данных.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Environment.Exit(-1);
-            }
-        }
-
-        private void open()
-        {
-            try
-            {
-                connection.Open();
-            }
-            catch (MySqlException e)
-            {
-                Console.WriteLine(e);
-                MessageBox.Show("Не удалось открыть подключение к базе данных.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Environment.Exit(-1);
-            }
-        }
-
-        public void disconnect()
-        {
-            connection.Close();
-        }
-
-        private MySqlConnection create()
-        {
-            string connectionData =
-                "database=" + database +
-                ";server=" + host +
-                ";uid=" + user +
-                ";pwd=" + password + ";";
-            return new MySqlConnection(connectionData);
-        }
-
-        private DBResult exec(string SQL)
-        {
-            DBResult result = new DBResult();
-            try
-            {
-                this.connect();
-
-                MySqlCommand query = new MySqlCommand(SQL, connection);
-                this.open();
-
-                MySqlDataReader reader = query.ExecuteReader();
-                if (reader.HasRows)
-                {
-                    result.success = true;
-                    int row = 0;
-                    while (reader.Read())
-                    {
-                        result.data.Add(new List<Object>());
-                        Object[] values = new Object[reader.FieldCount];
-                        int fieldCount = reader.GetValues(values);
-                        for (int i = 0; i < fieldCount; i++)
-                            result.data[row].Add(values[i]);
-                        row++;
-                        
-                    }
-                    reader.Close();
-                }
-                else
-                {
-                    result.error = "По запросу получено 0 записей.";
-                }
-                this.disconnect();
-            }
-            catch (MySqlException e)
-            {
-                Console.WriteLine(e);
-                MessageBox.Show("Не удалось выполнить запрос к базе данных.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Environment.Exit(-1);
-            }
-            catch (InvalidOperationException e)
-            {
-                Console.WriteLine(e);
-                MessageBox.Show("Не известная ошибка при исполнении запроса.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                Environment.Exit(-1);
-            }
-
-            return result;
-        }
-    }
+				dish = new detailDish { id = id, name = name };
+			}
+			return dish;
+		}
 
 
-    public class detailDish
-    {
-        public int id;
-        public string name;
-        public float proteins; // белки
-        public float fats; // жиры
-        public float carbohydrates; // углеводы
-        public float weight;
-        public float cost;
-    };
+		public void connect()
+		{
+			try
+			{
+				connection = this.create();
+			}
+			catch (MySqlException e)
+			{
+				Console.WriteLine(e);
+				MessageBox.Show("Не удалось создать подключение к базе данных.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				Environment.Exit(-1);
+			}
+		}
 
-    public class prewiewDish : List<Object>
-    {
-        public int id;
-        public string name;
-        public float weight;
-        public float cost;
-    }
+		private void open()
+		{
+			try
+			{
+				connection.Open();
+			}
+			catch (MySqlException e)
+			{
+				Console.WriteLine(e);
+				MessageBox.Show("Не удалось открыть подключение к базе данных.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				Environment.Exit(-1);
+			}
+		}
 
-    public class menuColumn
-    {
-        public string name;
-        public string header;
-        public int width;
-    }
-   
+		public void disconnect()
+		{
+			connection.Close();
+		}
+
+		private MySqlConnection create()
+		{
+			string connectionData =
+				"database=" + database +
+				";server=" + host +
+				";uid=" + user +
+				";pwd=" + password + ";";
+			return new MySqlConnection(connectionData);
+		}
+
+		private DBResult exec(string SQL)
+		{
+			DBResult result = new DBResult();
+			try
+			{
+				this.connect();
+
+				MySqlCommand query = new MySqlCommand(SQL, connection);
+				this.open();
+
+				MySqlDataReader reader = query.ExecuteReader();
+				if (reader.HasRows)
+				{
+					result.success = true;
+					int row = 0;
+					while (reader.Read())
+					{
+						result.data.Add(new List<Object>());
+						Object[] values = new Object[reader.FieldCount];
+						int fieldCount = reader.GetValues(values);
+						for (int i = 0; i < fieldCount; i++)
+							result.data[row].Add(values[i]);
+						row++;
+						
+					}
+					reader.Close();
+				}
+				else
+				{
+					result.error = "По запросу получено 0 записей.";
+				}
+				this.disconnect();
+			}
+			catch (MySqlException e)
+			{
+				Console.WriteLine(e);
+				MessageBox.Show("Не удалось выполнить запрос к базе данных.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				Environment.Exit(-1);
+			}
+			catch (InvalidOperationException e)
+			{
+				Console.WriteLine(e);
+				MessageBox.Show("Не известная ошибка при исполнении запроса.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				Environment.Exit(-1);
+			}
+
+			return result;
+		}
+	}
+
+	/**
+	 * Dish class for detail view
+	 * */
+	public class detailDish
+	{
+		public int id;
+		public string name;
+		public float proteins;
+		public float fats;
+		public float carbohydrates;
+		public float weight;
+		public float cost;
+	};
+
+	/**
+	 * Dish class for preview
+	 * */
+	public class prewiewDish : List<Object>
+	{
+		public int id;
+		public string name;
+		public float weight;
+		public float cost;
+	}
+
+	/**
+	 * Class-wrapper for tabs control
+	 * */
+	public class menuColumn
+	{
+		public string name;
+		public string header;
+		public int width;
+	}
 }
