2fa3b0813c2a862d56ab5e236475ef2d2be63d01
Abramov Anton <abramovantonru@ya.ru>
2017-03-08 15:20:46 +0300

    init project struct

---
+++ dataBase/src/js/core.js
@@ -0,0 +1,21 @@
+console.time('main');
+$(function () {
+
+	var App = (function () {
+		return{
+			init: function () {
+
+			}
+		}
+	})(),
+	Utils = (function () {
+		return{
+
+		}
+	})();
+
+	App.init();
+	console.log('jQuery ready');
+
+})();
+console.timeEnd('main');
